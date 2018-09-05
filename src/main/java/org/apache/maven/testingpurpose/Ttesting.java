141856b058044cf0e2cf6cbd044bd16362941cda
Kamal Kukreja <kamal.kukreja@soc-mac-76.local>
2018-09-05 16:11:25 +0530

    first commit

---
+++ src/main/java/org/apache/maven/testingpurpose/Ttesting.java
@@ -0,0 +1,13 @@
+package org.apache.maven.testingpurpose;
+
+import org.testng.annotations.Test;
+
+public class Ttesting {
+
+	
+	@Test
+	public void test1() {
+System.out.println("for testing");	
+}
+	
+}
---
+++ src/test/java/org/apache/maven/testingpurpose/calling.java
@@ -0,0 +1,11 @@
+package org.apache.maven.testingpurpose;
+
+public class calling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Ttesting test = new Ttesting();
+test.test1();
+}
+
+}
